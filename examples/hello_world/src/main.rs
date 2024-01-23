19458ddfd201d947c4d078745b88aa62e6f76b4a
saplyn <saplynx@outlook.com>
2024-01-23 20:39:37 +0800

    finish hello word example

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use yew::prelude::*;
+use yew_lmth::lmth;
+
+#[function_component]
+fn App() -> Html {
+    lmth!(
+        ! {
+            h1 { "Hello, world!" }
+            p { "This is an yew app with `lmth!{}` syntax." }
+        }
+    )
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
