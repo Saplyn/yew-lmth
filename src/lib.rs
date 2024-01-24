06d05f1c484aa0c6b1324854ac5f9c3ba83dd219
saplyn <saplynx@outlook.com>
2024-01-24 13:57:20 +0800

    format code

--- src/lib.rs
+++ src/lib.rs
@@ -47,37 +47,6 @@ mod tests;
 /// Macro for writing HTML-like syntax, which will be translated
 /// into a corresponding `yew::prelude::html!()` macro.
 ///
-/// # Syntax
-///
-/// ## Tags
-///
-/// | `lmth!` syntax        | meaning                         | `html!` sytax              |
-/// | --------------------- | ------------------------------- | -------------------------- |
-/// | `! { ... }`           | Yew's fragment                  | `<> ... </>`               |
-/// | `tag (attrs) { ... }` | Tag with attributes and content | `<tag attrs>{ ... }</tag>` |
-/// | `tag (attrs)`         | Void tag with attributes        | `<tag attrs />`            |
-/// | `tag { ... }`         | Tag with content                | `<tag>{ ... }</tag>`       |
-/// | `tag`                 | Void tag with no attribute      | `<tag />`                  |
-///
-/// ## Attributes
-///
-/// Attributes are separated by commas: `tag (attr: val, attr: val, ...) { ... }`
-///
-/// | `lmth!` syntax  | meaning                                | `html!` sytax  |
-/// | --------------- | -------------------------------------- | -------------- |
-/// | `attr: expr`    | Attribute with expression as value     | `attr={expr}`  |
-/// | `attr: {code}`  | Attribute with code block as value     | `attr={code}`  |
-/// | `attr="litstr"` | Attribute with literal string as value | `attr="litstr"`|
-/// | `attr`          | Shorthand for `{attr}` in yew          | `{attr}`       |
-///
-/// ## Content
-///
-/// | `lmth!` syntax  | meaning                   | `html!` sytax     |
-/// | --------------- | ------------------------- | ----------------- |
-/// | `{code}`        | Code as content           | `{code}`          |
-/// | `"litstr"`      | Literal string as content | `"litstr"`        |
-/// | `tag ...`       | Tag                       | corresponding tag |
-///
 /// # Example
 ///
 /// ```rust
@@ -87,7 +56,7 @@ mod tests;
 ///     div (class="container") {
 ///        h1 { "Hello, world!" }
 ///        button (onclick: handle_click()) { "Click me!" }
-///    }
+///     }
 /// }
 ///
 /// // expands to:
