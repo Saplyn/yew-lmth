dfe6c2d7fb5b4239d5b48afd090f342af1e92b80
saplyn <saplynx@outlook.com>
2024-01-28 22:47:48 +0800

    update cond example: `if let`

--- examples/cond/src/main.rs
+++ examples/cond/src/main.rs
@@ -4,6 +4,7 @@ use yew_lmth::lmth;
 #[function_component]
 fn App() -> Html {
     let cond = use_state(|| false);
+    let some_text = use_state(|| Some("there's some text".to_string()));
 
     lmth! {
         ! {
@@ -12,7 +13,21 @@ fn App() -> Html {
             } else {
                 p { "False" }
             }
-            button(onclick: move |_| cond.set(!*cond)) { "Toggle" }
+
+            if let Some(text) = &*some_text {
+                p { {text} }
+            } else {
+                p { "None" }
+            }
+
+            button(onclick: move |_| {
+                cond.set(!*cond);
+                if some_text.is_some() {
+                    some_text.set(None);
+                } else {
+                    some_text.set(Some("there's some text".to_string()));
+                }
+            }) { "Toggle" }
         }
     }
 }
